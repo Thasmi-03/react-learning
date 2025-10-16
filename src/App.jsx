4a944a649a67098f14d72346225afa3f30b11468
Thasmiga <vimalthasmiga1112@gmail.com>
2025-10-16 11:33:35 +0530

    page created compontens created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
 
 
 function App() {
 
   return (
     <>
-    <div></div>
+    <div className='main'> 
+      <Header/>
+      <Footer/>
+      <About/>
+      <Home/>
+    </div>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
-function Home (){
-
+function Home(){
+    return(
+        <>
+        <div className="main">
+            <h1>Home page</h1>
+        </div>
+        </>
+    )
 }
-
-
-
-
-
-
-
 
 
 export default Home;
